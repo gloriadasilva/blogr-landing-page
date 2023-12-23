6b829bd9a818995dabb4ba72dac983715bfdb0f1
Gloria <gloriadasilva620@gmail.com>
2023-12-23 14:58:28 +0100

    I added functionality to close button

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const connectArrow = document.querySelector(".connectArrow");
-const connectChildren = document.querySelector(".connectChildren")
-
+const connectChildren = document.querySelector(".connectChildren");
 
 connectArrow.addEventListener("click", function(){
    
    let clickin = connectChildren.classList.toggle("showChild");
 
-})
+});
+
 
 const menu = document.querySelector(".menu");
 const navList = document.querySelector(".navList");
-const close = document.querySelector(".close");
+const closed = document.querySelector(".closed");
 menu.addEventListener("click", function(){
    let clickMen = navList.classList.toggle("show");
 
+});
+// close btn
+
+closed.addEventListener("click", function(){
+    navList.classList.remove("show");
 });
